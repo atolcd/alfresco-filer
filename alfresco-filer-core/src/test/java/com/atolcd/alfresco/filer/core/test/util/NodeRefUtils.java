bb72363ec960096c4ae50b6e8cab0bfd065f2d1e
Corentin PIANA <cop@atolcd.com>
2019-08-29 15:52:14 +0200

    Test: Utility class for random NodeRef
    Build valid NodeRef with random ID in chosed Sapces store
    
    Change-Id: Ica55c2d90d47e1ce2c36cab0ad4ab21ecadc46af

---
+++ alfresco-filer-core/src/test/java/com/atolcd/alfresco/filer/core/test/util/NodeRefUtils.java
@@ -0,0 +1,23 @@
+package com.atolcd.alfresco.filer.core.test.util;
+
+import java.util.UUID;
+
+import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.repository.StoreRef;
+
+public final class NodeRefUtils {
+
+  public static NodeRef randomNodeRef() {
+    return randomWorkspaceSpacesStoreNodeRef();
+  }
+
+  public static NodeRef randomWorkspaceSpacesStoreNodeRef() {
+    return new NodeRef(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE, UUID.randomUUID().toString());
+  }
+
+  public static NodeRef randomArchiveSpacesStoreNodeRef() {
+    return new NodeRef(StoreRef.STORE_REF_ARCHIVE_SPACESSTORE, UUID.randomUUID().toString());
+  }
+
+  private NodeRefUtils() {}
+}
