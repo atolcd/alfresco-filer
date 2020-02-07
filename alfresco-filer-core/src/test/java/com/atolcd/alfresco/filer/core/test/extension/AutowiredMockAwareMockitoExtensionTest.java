fef6f35f4c14f1542351948d2812b1a49600fe8f
Nicolas Barithel <n.barithel@atolcd.com>
2020-02-07 10:03:42 +0100

    Update JUnit 5.6.0
    Change-Id: If54133ce7e6f9d3efcdaccbd15cb9a6c909dfed1

--- alfresco-filer-core/src/test/java/com/atolcd/alfresco/filer/core/test/extension/AutowiredMockAwareMockitoExtensionTest.java
+++ alfresco-filer-core/src/test/java/com/atolcd/alfresco/filer/core/test/extension/AutowiredMockAwareMockitoExtensionTest.java
@@ -44,7 +44,7 @@ public class AutowiredMockAwareMockitoExtensionTest {
 
   @Test
   public void necessaryStubbing() {
-    Events testEvents = executeTestsForClass(NecessaryStubbingTestClass.class).tests();
+    Events testEvents = executeTestsForClass(NecessaryStubbingTestClass.class).testEvents();
 
     logTestFailures(testEvents);
 
@@ -80,7 +80,7 @@ public class AutowiredMockAwareMockitoExtensionTest {
 
   @Test
   public void unnecessaryStubbing() {
-    Events testEvents = executeTestsForClass(UnnecessaryStubbingTestClass.class).tests();
+    Events testEvents = executeTestsForClass(UnnecessaryStubbingTestClass.class).testEvents();
 
     logTestFailures(testEvents);
 
@@ -115,7 +115,7 @@ public class AutowiredMockAwareMockitoExtensionTest {
 
   @Test
   public void stubbing() {
-    Events testEvents = executeTestsForClass(StubbingTestClass.class).tests();
+    Events testEvents = executeTestsForClass(StubbingTestClass.class).testEvents();
 
     logTestFailures(testEvents);
 
